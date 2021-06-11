6bddd3a37c9473cf7f1ca163fbd6181c1fb813f4
Roy Cohen <rtcohen99@gmail.com>
2021-06-11 13:05:39 +0000

    Add a read csv function that tokenizes the words and sentences in a csv file

--- utils.py
+++ utils.py
@@ -3,6 +3,40 @@ import os
 from collections import defaultdict
 import csv
 
+def read_csv(filename):
+    """Reads a CSV file and iterates through it extracts each sentence's words from the file.
+        :param filename: The name of the CSV file to read from.
+        
+        :return sentences: A 2d array of individual words and of each sentence, including start and end words.
+    """
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, filename)
+    sentences = []
+    sen = []
+    sen.append("<s>")
+    word_counts = 1;
+    with open(filename) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=",")
+        for i, row in enumerate(csv_reader):
+            if i == 0:
+                continue
+            else:
+                if row[0] == '':
+                    sen.append("</s>")
+                    word_count += 1
+                    sentences.append(sen)
+
+                    sen = []
+                    sen.append("<s>")
+                    word_count += 1
+                else:
+                    sen.append((row[0].lower()))
+                    word_count += 1
+    sen.append("</s>")
+    word_count += 1
+    sentences.append(sen)
+    return sentences, word_counts
+
 def convert_to_unk(sentences, word_counts, threshold):
     """Iterates through each word in each sentences and replaces words with a frequency lower then the threshold to the word UNK.
 
