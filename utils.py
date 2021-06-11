952685b09f97c9ce71b1057bf89f2f4f1b8e59da
Edan Toledo <TLDEDA001@myuct.ac.za>
2021-06-11 15:03:05 +0000

    fix util errors

--- utils.py
+++ utils.py
@@ -3,9 +3,9 @@ import os
 from collections import defaultdict
 import csv
 
-def read_csv(filename):
-    """Reads a CSV file and iterates through it extracts each sentence's words from the file.
-        :param filename: The name of the CSV file to read from.
+def read_file(filename):
+    """Reads a file and iterates through it extracts each sentence's words from the file.
+        :param filename: The name of the file to read from.
         
         :return sentences: A 2d array of individual words and of each sentence, including start and end words.
         :return word_counts: A dict of word to integer, which is the frequency of each words occurence.
@@ -14,29 +14,22 @@ def read_csv(filename):
     filename = os.path.join(dirname, filename)
     sentences = []
     sen = []
-    sen.append("<s>")
-    word_count = defaultdict(int)
-    word_counts["<s>"] += 1;
-    with open(filename) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=",")
-        for i, row in enumerate(csv_reader):
-            if i == 0:
-                continue
-            else:
-                if row[0] == '':
-                    sen.append("</s>")
-                    word_count["</s>"] += 1
-                    sentences.append(sen)
-
-                    sen = []
-                    sen.append("<s>")
-                    word_count["<s>"] += 1
-                else:
-                    sen.append((row[0].lower()))
-                    word_count[row[0].lower()] += 1
-    sen.append("</s>")
-    word_count["</s>"] += 1
-    sentences.append(sen)
+    word_counts = defaultdict(int)
+    
+    with open(filename) as f:
+        for line in f:
+           
+            sen.append("<s>")
+            word_counts["<s>"] += 1
+            words = line.split(" ")
+            for word in words:
+                sen.append(word.lower())
+                word_counts[word.lower()] += 1
+            sen.append("</s>")
+            word_counts["</s>"] += 1 
+            sentences.append(sen)
+            sen = []   
+            
     return sentences, word_counts
 
 def convert_to_unk(sentences, word_counts, threshold):
@@ -51,11 +44,11 @@ def convert_to_unk(sentences, word_counts, threshold):
     new_sentences = []
     for sentence in sentences:
         new_sentence = []
-        for (word, tag) in sentence:
+        for word in sentence:
             if word_counts[word] > threshold:
-                new_sentence.append((word, tag))
+                new_sentence.append(word)
             else:
-                new_sentence.append(("<UNK>", tag))
+                new_sentence.append("<UNK>")
         new_sentences.append(new_sentence)
 
     return new_sentences
@@ -70,7 +63,7 @@ def create_word_ids(sentences):
     current_id = 0
     word_to_id = {}
     for sentence in sentences:
-        for word in sentences:
+        for word in sentence:
             if not (word in word_to_id):
                 word_to_id[word] = current_id;
                 current_id += 1
@@ -82,21 +75,19 @@ def create_n_gram(sentences,n):
     :param sentences: A 2d array of individual words and of each sentence
     :param n: The size of the n-gram
 
-    :return n_gram: An array of tuples in the form of (array of previous words, word).
+    :return ngram: An array of tuples in the form of (array of previous words, word).
     """
-    n_gram = []
+    ngram = []
     for sentence in sentences:
         prev_words = []
-        for (int i = 0; i<n;i++){
-            prev_words.append([)sentence[i])
-        }
-        ngram.append((prev_words,sentence[n]))
-        for (int = n; i<len(sentence)-1; i++){
-            prev_words.pop()
+        for i in range (0,n-1):
+            prev_words.append(sentence[i])
+        ngram.append((prev_words,sentence[n-1]))
+        for i in range (n, len(sentence)-1):
+            prev_words.pop(0)
             prev_words.append(sentence[i])
             ngram.append((prev_words,sentence[i+1]))
-        }
+        
     return ngram
     
-
 
